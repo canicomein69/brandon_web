3cc55d4478c5615caa1ec4bda770673a4186ca20
brandy69 <bleonb690@gmail.com>
2023-09-02 16:26:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -81,7 +81,7 @@ def MyCrud():
         html.img(
         {
             "src": "https://picsum.photos/id/274/500/300",
-            "style": {"width": "150%"},
+            "style": {"width": "100%" , "height":"50%"}, 
             "alt": "Ray Charles",
         }
     )
